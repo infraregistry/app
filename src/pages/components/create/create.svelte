74a5d31411b3f111567fb0031723169f58cef43a
Matthew Davis <matthew@matthewdavis.io>
2024-10-12 13:29:46 -0500

    bump

--- src/pages/components/create/create.svelte
+++ src/pages/components/create/create.svelte
@@ -1,1 +1,13 @@
-<div class="h-96 w-96 bg-red-500">Create component dialog</div>
+<script lang="ts">
+  import { type ModalInstance } from "@mateothegreat/svelte5-modal-manager";
+
+  let { instance }: { instance: ModalInstance } = $props();
+
+  console.log(instance);
+</script>
+
+<div class="h-96 w-96 bg-red-500">
+  Create component dialog
+
+  <button on:click={() => instance.manager.close(instance.config.id)}>Close</button>
+</div>
