4e589732c1f571dec2311b95bf8c343c1de3a825
Matthew Davis <matthew@matthewdavis.io>
2024-10-24 08:22:59 -0500

    nav

--- src/lib/nav/vertical-nav.svelte
+++ src/lib/nav/vertical-nav.svelte
@@ -1,152 +1,87 @@
 <script lang="ts">
-  import { Home, LineChart, Package, Package2, ShoppingCart, UsersRound } from "lucide-svelte";
+  import * as DropdownMenu from "$lib/components/ui/dropdown-menu";
 
-  import { openCommandPalette } from "$lib/components/command-palette/command-palette-types.svelte";
-  import { Button } from "$lib/components/ui/button";
-  import { DropdownMenu, DropdownMenuTrigger } from "$lib/components/ui/dropdown-menu";
-  import DropdownMenuContent from "$lib/components/ui/dropdown-menu/dropdown-menu-content.svelte";
-  import DropdownMenuItem from "$lib/components/ui/dropdown-menu/dropdown-menu-item.svelte";
-  import * as Sheet from "$lib/components/ui/sheet";
   import * as Tooltip from "$lib/components/ui/tooltip/index.js";
-  import { sessions } from "$lib/sessions/sessions.svelte";
-  import { LogOutIcon, PanelLeft, Search, UserCircle } from "lucide-svelte";
+  import type { iconStatics } from "$lib/shared/icons/icon-statics";
+  import IconifyWrapper from "$lib/shared/icons/iconify-wrapper.svelte";
   import { link } from "svelte-spa-router";
+
+  type Link = {
+    href: string;
+    icon: keyof typeof iconStatics;
+    tooltip: string;
+  };
+
+  const links: Link[] = [
+    { href: "/catalog", icon: "catalog", tooltip: "Service Catalog" },
+    { href: "/components", icon: "components", tooltip: "Components" },
+    { href: "/cmdb", icon: "cmdb", tooltip: "Change Management Database" },
+    { href: "/components", icon: "components", tooltip: "Components" }
+  ];
 </script>
 
-<aside class="fixed inset-y-0 left-0 z-10 hidden w-14 flex-col border-r bg-background sm:flex">
-  <nav class="flex flex-col items-center gap-4 px-2 py-4">
-    <a href="##" class="group flex h-9 w-9 shrink-0 items-center justify-center gap-2 rounded-full bg-primary text-lg font-semibold text-primary-foreground md:h-8 md:w-8 md:text-base">
-      <Package2 class="h-4 w-4 transition-all group-hover:scale-110" />
-      <span class="sr-only">Acme Inc</span>
+<aside class="fixed inset-y-0 left-0 z-10 hidden w-14 flex-col border-r bg-background pt-4 sm:flex">
+  <nav class="flex flex-col items-center gap-4">
+    <a use:link href="/">
+      <IconifyWrapper name="logo" size={9} />
     </a>
-    <Tooltip.Root on:click={() => openCommandPalette({ data: [] })}>
-      <Tooltip.Trigger asChild let:builder>
-        <div class="flex h-9 w-9 items-center justify-center rounded-lg text-muted-foreground transition-colors hover:text-foreground md:h-8 md:w-8" use:builder.action {...builder}>
-          <Search class="h-5 w-5" />
-        </div>
-      </Tooltip.Trigger>
-      <Tooltip.Content side="right">Dashboard</Tooltip.Content>
-    </Tooltip.Root>
-    <Tooltip.Root>
-      <Tooltip.Trigger asChild let:builder>
-        <a href="##" class="flex h-9 w-9 items-center justify-center rounded-lg text-muted-foreground transition-colors hover:text-foreground md:h-8 md:w-8" use:builder.action {...builder}>
-          <Home class="h-5 w-5" />
-          <span class="sr-only">Dashboard</span>
-        </a>
-      </Tooltip.Trigger>
-      <Tooltip.Content side="right">Dashboard</Tooltip.Content>
-    </Tooltip.Root>
-    <Tooltip.Root>
-      <Tooltip.Trigger asChild let:builder>
-        <a use:link={"/components"} class="flex h-9 w-9 items-center justify-center rounded-lg bg-accent text-accent-foreground transition-colors hover:text-foreground md:h-8 md:w-8" use:builder.action {...builder}>
-          <ShoppingCart class="h-5 w-5" />
-          <span class="sr-only">Orders</span>
-        </a>
-      </Tooltip.Trigger>
-      <Tooltip.Content side="right">Orders</Tooltip.Content>
-    </Tooltip.Root>
-    <Tooltip.Root>
-      <Tooltip.Trigger asChild let:builder>
-        <a href="##" class="flex h-9 w-9 items-center justify-center rounded-lg text-muted-foreground transition-colors hover:text-foreground md:h-8 md:w-8" use:builder.action {...builder}>
-          <Package class="h-5 w-5" />
-          <span class="sr-only">Products</span>
-        </a>
-      </Tooltip.Trigger>
-      <Tooltip.Content side="right">Products</Tooltip.Content>
-    </Tooltip.Root>
-    <Tooltip.Root>
-      <Tooltip.Trigger asChild let:builder>
-        <a href="##" class="flex h-9 w-9 items-center justify-center rounded-lg text-muted-foreground transition-colors hover:text-foreground md:h-8 md:w-8" use:builder.action {...builder}>
-          <UsersRound class="h-5 w-5" />
-          <span class="sr-only">Customers</span>
-        </a>
-      </Tooltip.Trigger>
-      <Tooltip.Content side="right">Customers</Tooltip.Content>
-    </Tooltip.Root>
-    <Tooltip.Root>
-      <Tooltip.Trigger asChild let:builder>
-        <a href="##" class="flex h-9 w-9 items-center justify-center rounded-lg text-muted-foreground transition-colors hover:text-foreground md:h-8 md:w-8" use:builder.action {...builder}>
-          <LineChart class="h-5 w-5" />
-          <span class="sr-only">Analytics</span>
-        </a>
-      </Tooltip.Trigger>
-      <Tooltip.Content side="right">Analytics</Tooltip.Content>
-    </Tooltip.Root>
+    <div class="h-px w-full bg-slate-800" />
   </nav>
-  <nav class="mt-auto flex flex-col items-center gap-4 px-2 py-4">
+  <nav class="flex flex-1 flex-col items-center gap-3.5 px-2 py-4">
+    {#each links as l}
+      <Tooltip.Root>
+        <Tooltip.Trigger asChild let:builder>
+          <a use:link href={l.href} class="" use:builder.action {...builder}>
+            <IconifyWrapper name={l.icon} size={7} />
+          </a>
+        </Tooltip.Trigger>
+        <Tooltip.Content side="right">{l.tooltip}</Tooltip.Content>
+      </Tooltip.Root>
+    {/each}
+  </nav>
+  <nav class="flex flex-col items-center gap-3.5 border-t border-slate-800 py-3">
     <Tooltip.Root>
       <Tooltip.Trigger asChild let:builder>
-        <DropdownMenu>
-          <DropdownMenuTrigger>
-            <Button variant="ghost" class="rounded-full border px-2 py-2 text-muted-foreground">
-              <UserCircle size={16} />
-            </Button>
-          </DropdownMenuTrigger>
-          <DropdownMenuContent>
-            <DropdownMenuItem
-              onclick={() => {
-                sessions.logout();
-              }}>
-              <div class="flex flex-row items-center gap-2">
-                <LogOutIcon size={12} />
-                <span class="text-xs text-muted-foreground">Logout</span>
-              </div>
-            </DropdownMenuItem>
-          </DropdownMenuContent>
-        </DropdownMenu>
+        <DropdownMenu.Root>
+          <DropdownMenu.Trigger>
+            <IconifyWrapper name="help" size={7} />
+          </DropdownMenu.Trigger>
+          <DropdownMenu.Content>
+            <DropdownMenu.Group>
+              <DropdownMenu.Label>My Account</DropdownMenu.Label>
+              <DropdownMenu.Separator />
+              <DropdownMenu.Item>Profile</DropdownMenu.Item>
+              <DropdownMenu.Item>Billing</DropdownMenu.Item>
+              <DropdownMenu.Item>Team</DropdownMenu.Item>
+              <DropdownMenu.Item>Subscription</DropdownMenu.Item>
+            </DropdownMenu.Group>
+          </DropdownMenu.Content>
+        </DropdownMenu.Root>
+      </Tooltip.Trigger>
+      <Tooltip.Content side="right">Settings</Tooltip.Content>
+    </Tooltip.Root>
+    <IconifyWrapper name="settings" size={7} />
+  </nav>
+  <nav class="flex flex-col items-center gap-3.5 border-t border-slate-800 py-3">
+    <Tooltip.Root>
+      <Tooltip.Trigger asChild let:builder>
+        <DropdownMenu.Root>
+          <DropdownMenu.Trigger>
+            <IconifyWrapper name="user" size={8} />
+          </DropdownMenu.Trigger>
+          <DropdownMenu.Content>
+            <DropdownMenu.Group>
+              <DropdownMenu.Label>My Account</DropdownMenu.Label>
+              <DropdownMenu.Separator />
+              <DropdownMenu.Item>Profile</DropdownMenu.Item>
+              <DropdownMenu.Item>Billing</DropdownMenu.Item>
+              <DropdownMenu.Item>Team</DropdownMenu.Item>
+              <DropdownMenu.Item>Subscription</DropdownMenu.Item>
+            </DropdownMenu.Group>
+          </DropdownMenu.Content>
+        </DropdownMenu.Root>
       </Tooltip.Trigger>
       <Tooltip.Content side="right">Settings</Tooltip.Content>
     </Tooltip.Root>
   </nav>
 </aside>
-
-<Sheet.Root>
-  <Sheet.Trigger asChild let:builder>
-    <Button builders={[builder]} size="icon" variant="outline" class="sm:hidden">
-      <PanelLeft class="h-5 w-5" />
-      <span class="sr-only">Toggle Menu</span>
-    </Button>
-  </Sheet.Trigger>
-  <Sheet.Content side="left" class="sm:max-w-xs">
-    <nav class="grid gap-6 text-lg font-medium">
-      <a href="##" class="group flex h-10 w-10 shrink-0 items-center justify-center gap-2 rounded-full bg-primary text-lg font-semibold text-primary-foreground md:text-base">
-        <Package2 class="h-5 w-5 transition-all group-hover:scale-110" />
-        <span class="sr-only">Acme Inc</span>
-      </a>
-      <a href="##" class="flex items-center gap-4 px-2.5 text-muted-foreground hover:text-foreground">
-        <Home class="h-5 w-5" />
-        Dashboard
-      </a>
-      <a href="##" class="flex items-center gap-4 px-2.5 text-foreground">
-        <ShoppingCart class="h-5 w-5" />
-        Orders
-      </a>
-      <a href="##" class="flex items-center gap-4 px-2.5 text-muted-foreground hover:text-foreground">
-        <Package class="h-5 w-5" />
-        Products
-      </a>
-      <a href="##" class="flex items-center gap-4 px-2.5 text-muted-foreground hover:text-foreground">
-        <UsersRound class="h-5 w-5" />
-        Customers
-      </a>
-      <DropdownMenu>
-        <DropdownMenuTrigger>
-          <Button variant="ghost" class="rounded-full border px-2 py-2 text-muted-foreground">
-            <UserCircle size={16} />
-          </Button>
-        </DropdownMenuTrigger>
-        <DropdownMenuContent>
-          <DropdownMenuItem
-            onclick={() => {
-              sessions.logout();
-            }}>
-            <div class="flex flex-row items-center gap-2">
-              <LogOutIcon size={12} />
-              <span class="text-xs text-muted-foreground">Logout</span>
-            </div>
-          </DropdownMenuItem>
-        </DropdownMenuContent>
-      </DropdownMenu>
-    </nav>
-  </Sheet.Content>
-</Sheet.Root>
--- src/lib/shared/icons/icon-statics.ts
+++ src/lib/shared/icons/icon-statics.ts
@@ -1,4 +1,8 @@
 export const iconStatics = {
+  logo: {
+    value: "game-icons:mesh-network",
+    classes: "text-gray-600",
+  },
   help: {
     value: "ic:twotone-help",
     classes: "text-fuchsia-500",
@@ -10,5 +14,29 @@ export const iconStatics = {
   relationshipAdd: {
     value: "fluent:link-add-16-filled",
     classes: "text-slate-600",
+  },
+  overview: {
+    value: "material-symbols:overview-key-outline",
+    classes: "text-slate-600",
+  },
+  components: {
+    value: "tdesign:system-components",
+    classes: "text-slate-600 hover:text-green-600",
+  },
+  cmdb: {
+    value: "tabler:database-cog",
+    classes: "text-slate-600 hover:text-green-600",
+  },
+  catalog: {
+    value: "grommet-icons:catalog",
+    classes: "text-indigo-600 hover:text-green-600",
+  },
+  user: {
+    value: "duo-icons:user-card",
+    classes: "text-emerald-500 hover:text-green-600",
+  },
+  settings: {
+    value: "streamline:ai-settings-spark",
+    classes: "text-slate-600 hover:text-green-600",
   },
 };
