5f12fce7e60e5ebeac8c41519dcd6adb0017a8f5
roaming97 <roaming98a@gmail.com>
2024-10-24 14:24:04 -0600

    Views for registration steps

--- src/pages/sessions/register/register.svelte
+++ src/pages/sessions/register/register.svelte
@@ -1,21 +1,32 @@
 <script>
+  import Button from "$lib/components/ui/button/button.svelte";
+  import BrandCard from "$lib/sessions/BrandCard.svelte";
   import AuthForm from "../../components/sessions/AuthForm.svelte";
   import { FormContext } from "../../components/sessions/sessions.type";
 </script>
 
-<div class="lg:p-8">
-  <div class="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
-    <div class="flex flex-col space-y-2 text-center">
-      <h1 class="text-2xl font-semibold tracking-tight">Create an account</h1>
-      <p class="text-muted-foreground text-sm">Enter your email below to create your account</p>
+<div class="flex h-screen w-full items-center">
+  <BrandCard />
+  <div class="mx-auto lg:p-8">
+    <div class="absolute right-12 top-10">
+      <Button href="/#/login" variant="ghost" size="sm">Login</Button>
     </div>
-    <AuthForm context={FormContext.REGISTER} />
-    <p class="text-muted-foreground px-8 text-center text-sm">
-      By clicking continue, you agree to our
-      <a href="/terms" class="hover:text-primary underline underline-offset-4"> Terms of Service </a>
-      and
-      <a href="/privacy" class="hover:text-primary underline underline-offset-4"> Privacy Policy </a>
-      .
-    </p>
+    <div class="mx-auto flex w-full flex-col space-y-6 sm:w-[350px]">
+      <div class="flex flex-col space-y-2">
+        <h1 class="text-2xl font-semibold tracking-tight">Tell us how to reach you...</h1>
+        <p class="text-sm text-muted-foreground">
+          Register for your free infraregistry account with just an email address!
+          <b> No credit card required. </b>
+        </p>
+      </div>
+      <AuthForm context={FormContext.REGISTER} />
+      <p class="px-8 text-center text-sm text-muted-foreground">
+        By clicking continue, you agree to our
+        <a href="/terms" class="underline underline-offset-4 hover:text-primary"> Terms of Service </a>
+        and
+        <a href="/privacy" class="underline underline-offset-4 hover:text-primary"> Privacy Policy </a>
+        .
+      </p>
+    </div>
   </div>
 </div>
---
+++ src/pages/sessions/reset/reset.svelte
@@ -0,0 +1,41 @@
+<script>
+  import Button from "$lib/components/ui/button/button.svelte";
+  import Input from "$lib/components/ui/input/input.svelte";
+  import Label from "$lib/components/ui/label/label.svelte";
+  import BrandCard from "$lib/sessions/BrandCard.svelte";
+
+  // TODO: maybe make this a store later on
+  let codeSent = $state(false);
+</script>
+
+<div class="flex h-screen w-full items-center">
+  <BrandCard />
+  <div class="mx-auto lg:p-8">
+    <div class="absolute right-12 top-10 flex items-center gap-1">
+      <Button href="/#/register" variant="ghost" size="sm">Register</Button>
+      <Button href="/#/login" variant="ghost" size="sm">Login</Button>
+    </div>
+    <div class="mx-auto flex w-full flex-col space-y-2 sm:w-[350px]">
+      {#if !codeSent}
+        <div class="flex flex-col space-y-2">
+          <h1 class="text-2xl font-semibold tracking-tight">Reset your password..</h1>
+          <p class="text-sm text-muted-foreground">We just need to send you a 6-digit code, please enter the email associated with your account below:</p>
+          <Label class="sr-only" for="email">Email</Label>
+          <Input id="email" placeholder="name@example.com" type="email" autocapitalize="none" autocomplete="email" autocorrect="off" />
+        </div>
+        <Button type="submit" onclick={() => (codeSent = true)}>Send Password Reset</Button>
+      {:else}
+        <div class="flex flex-col space-y-2">
+          <h1 class="text-2xl font-semibold tracking-tight">Enter verification code..</h1>
+          <p class="text-sm text-muted-foreground">We sent a 6-digit code to name@example.com. Please copy that code and input it below or click the link in the message:</p>
+          <Label class="sr-only" for="code">Code</Label>
+          <Input id="text" placeholder="AZQ-WDG" type="text" autocapitalize="none" autocorrect="off" />
+        </div>
+        <Button type="submit">Confirm</Button>
+        <div class="ml-auto">
+          <Button variant="ghost" size="sm">Resend</Button>
+        </div>
+      {/if}
+    </div>
+  </div>
+</div>
