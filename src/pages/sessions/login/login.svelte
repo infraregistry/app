9d68081437b8b49ac439f4eee6b2e90ee7c4ca19
Matthew Davis <matthew@matthewdavis.io>
2024-10-25 16:04:19 -0500

    login + logout

--- src/pages/sessions/login/login.svelte
+++ src/pages/sessions/login/login.svelte
@@ -1,17 +1,35 @@
 <script lang="ts">
   import Button from "$lib/components/ui/button/button.svelte";
   import BrandCard from "$lib/sessions/BrandCard.svelte";
+  import { sessions } from "$lib/sessions/sessions.svelte";
   import * as Form from "@mateothegreat/svelte5-forms";
   import { FormInstance, minLength } from "@mateothegreat/svelte5-forms";
+  import { GithubIcon, LoaderCircleIcon } from "lucide-svelte";
+  import { toast } from "svelte-sonner";
+  import { push } from "svelte-spa-router";
 
   let form: FormInstance;
 
   const controls = $state([
     {
       name: "email",
-      validators: [minLength(3)]
+      validators: [minLength(8)]
+    },
+    {
+      name: "password",
+      validators: [minLength(8)]
     }
   ]);
+
+  let isLoading = $state<boolean | "email" | "github" | "google">(false);
+
+  const login = () => {
+    isLoading = "email";
+    sessions.login(form.values.email, form.values.password).subscribe((result) => {
+      toast.success("Welcome back!");
+      push("/");
+    });
+  };
 </script>
 
 <div class="flex h-screen w-full items-center">
@@ -30,41 +48,56 @@
         </p>
       </div>
       <div class="">
-        <Form.Root bind:form {controls}>
-          <Form.Group>
-            <Form.Field>
-              <Form.Header.Root>
-                <Form.Header.Label>Name</Form.Header.Label>
-                <Form.Header.Description>Foo bar baz pow.</Form.Header.Description>
-              </Form.Header.Root>
-              <Form.Controls.Input {form} name="name" />
-            </Form.Field>
-            <Form.Field>
-              <Form.Header.Root>
-                <Form.Header.Label>Foo Bar</Form.Header.Label>
-                <Form.Header.Description>Foo bar baz pow.</Form.Header.Description>
-              </Form.Header.Root>
-              <Form.Controls.Select {form} placeholder="Select something cool.." name="foobar" type="multiple" prefix={selectPrefix} />
-            </Form.Field>
-          </Form.Group>
-          <Form.Group>
-            <Form.Field>
-              <Form.Header.Root>
-                <Form.Header.Label>Name</Form.Header.Label>
-                <Form.Header.Description>Foo bar baz pow.</Form.Header.Description>
-              </Form.Header.Root>
-              <Form.Controls.Switch {form} name="agree" class="rounded-lg border-2 border-slate-800 bg-zinc-900/40 p-2">
-                <span class="text-xs text-sky-500">I agree to be awesome.</span>
-              </Form.Controls.Switch>
-            </Form.Field>
-          </Form.Group>
-        </Form.Root>
+        <div class="grid">
+          <div class="grid gap-2">
+            <Form.Root bind:form {controls}>
+              <Form.Group>
+                <Form.Field>
+                  <Form.Controls.Input {form} name="email" placeholder="Enter your email address.." />
+                  <Form.Controls.Input {form} name="password" placeholder="Enter your password.." type="password" />
+                </Form.Field>
+              </Form.Group>
+            </Form.Root>
+            <Button disabled={!form.valid || !form.values.email || isLoading} onclick={login}>
+              {#if isLoading}
+                <LoaderCircleIcon class="mr-2 h-4 w-4 animate-spin" />
+              {/if}
+              Sign In with Email
+            </Button>
+          </div>
+          <div class="relative my-4">
+            <div class="absolute inset-0 flex items-center">
+              <span class="w-full border-t"></span>
+            </div>
+            <div class="relative flex justify-center text-xs uppercase">
+              <span class="rounded-sm bg-background px-2 font-bold text-muted-foreground"> Or continue with </span>
+            </div>
+          </div>
+          <div class="flex flex-col gap-4">
+            <Button variant="outline" type="button" disabled={isLoading}>
+              {#if isLoading === "google"}
+                <LoaderCircleIcon class="mr-2 h-4 w-4 animate-spin" />
+              {:else}
+                <GithubIcon class="mr-2 h-4 w-4" />
+              {/if}
+              Google
+            </Button>
+            <Button variant="outline" type="button" disabled={isLoading}>
+              {#if isLoading === "github"}
+                <LoaderCircleIcon class="mr-2 h-4 w-4 animate-spin" />
+              {:else}
+                <GithubIcon class="mr-2 h-4 w-4" />
+              {/if}
+              GitHub
+            </Button>
+          </div>
+        </div>
       </div>
       <p class="px-8 text-center text-sm text-muted-foreground">
         By clicking continue, you agree to our
-        <a href="/terms" class="underline underline-offset-4 hover:text-primary"> Terms of Service </a>
+        <a href="https://infraregistry.com/terms" target="_blank" class="underline underline-offset-4 hover:text-primary"> Terms of Service </a>
         and
-        <a href="/privacy" class="underline underline-offset-4 hover:text-primary"> Privacy Policy </a>
+        <a href="https://infraregistry.com/privacy" target="_blank" class="underline underline-offset-4 hover:text-primary"> Privacy Policy </a>
         .
       </p>
     </div>
