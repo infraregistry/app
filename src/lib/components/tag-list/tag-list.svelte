fe07b7f8f07192adfea1a122da01236c89911e8f
roaming97 <roaming98a@gmail.com>
2024-10-29 15:41:24 -0600

    Add newly created tags to filtered list

--- src/lib/components/tag-list/tag-list.svelte
+++ src/lib/components/tag-list/tag-list.svelte
@@ -4,7 +4,6 @@
   import type { Tag } from "$lib/types";
   import { Command } from "bits-ui";
   import { noop } from "rxjs";
-  import { writable } from "svelte/store";
   import Button from "../ui/button/button.svelte";
   import Input from "../ui/input/input.svelte";
   import BadgeTag from "./badge-tag.svelte";
@@ -22,8 +21,8 @@
   let { tags, disabled = false, mode = "button", search = true, max = 10, onValueChange = noop, wrap = true }: Props = $props();
 
   let filtered = $state(tags);
-  let terms = writable("");
-  let createable = $derived($terms.length > 0);
+  let terms = $state("");
+  let createable = $derived(terms.length > 0);
   let wrapClass = $derived(wrap ? "flex-wrap" : "overflow-x-auto");
 
   const add = (tag: Tag) => {
@@ -56,7 +55,7 @@
         <Popover.Content class="w-full p-0">
           <Command.Root class="flex w-56 flex-col self-start overflow-hidden rounded-xl bg-background">
             {#if search}
-              <Command.Input placeholder="Search tags..." bind:value={$terms} disabled={tags.length >= max} class="focus-override h-input placeholder:text-foreground-alt/50 inline-flex w-[296px] truncate rounded-xl bg-background px-4 py-2.5 text-sm transition-colors focus:outline-none focus:ring-0" />
+              <Command.Input placeholder="Search tags..." bind:value={terms} disabled={tags.length >= max} class="focus-override h-input placeholder:text-foreground-alt/50 inline-flex w-[296px] truncate rounded-xl bg-background px-4 py-2.5 text-sm transition-colors focus:outline-none focus:ring-0" />
             {/if}
             <Command.List class="max-h-[200px] overflow-y-auto overflow-x-hidden">
               <Command.Viewport>
@@ -66,20 +65,21 @@
                     class="flex h-8 w-full cursor-pointer items-center gap-3 bg-green-500 text-white transition-colors hover:bg-green-600"
                     onclick={() => {
                       add({
-                        label: $terms,
+                        label: terms,
                         color: "bg-gray-500",
                         disabled: false
                       });
-                      terms.set("");
+                      filtered = tags;
+                      terms = "";
                     }}>
-                    Create tag "{$terms}"
+                    Create tag "{terms}"
                   </Button>
                 </Command.Empty>
                 <Command.Group>
                   {#if filtered.length > 0}
                     {#each filtered as suggestion}
                       <Command.Item class="rounded-button flex h-10 cursor-pointer select-none items-center gap-2 px-3 py-2.5 text-sm capitalize outline-none data-[selected]:bg-muted">
-                        <button class="flex h-8 cursor-pointer items-center gap-2 transition-colors hover:bg-muted" disabled={suggestion.disabled} onclick={() => add(suggestion)}>
+                        <button class="flex h-8 items-center gap-2 transition-colors hover:enabled:cursor-pointer hover:enabled:bg-muted" disabled={suggestion.disabled || tags.map((t) => t.label).includes(suggestion.label)} onclick={() => add(suggestion)}>
                           <div class="h-4 w-4 rounded-md {suggestion.color}"></div>
                           <span class="select-none text-sm">{suggestion.label}</span>
                         </button>
