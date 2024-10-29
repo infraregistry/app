e23baf843dca3b1d421d1dcf484552fd5cd7d254
roaming97 <roaming98a@gmail.com>
2024-10-28 18:49:43 -0600

    Finalize tag list

--- src/lib/components/tag-list/tag-list.svelte
+++ src/lib/components/tag-list/tag-list.svelte
@@ -1,11 +1,12 @@
 <script lang="ts">
   import BadgeTag from "./badge-tag.svelte";
-  import * as DropdownMenu from "$lib/components/ui/dropdown-menu";
+  import * as Command from "$lib/components/ui/command";
   import * as Card from "$lib/components/ui/card";
-  import Input from "$lib/components/ui/input/input.svelte";
+  import * as Popover from "$lib/components/ui/popover";
   import type { Tag } from "$lib/types";
   import { noop } from "rxjs";
-  import { buttonVariants } from "../ui/button";
+  import Button from "../ui/button/button.svelte";
+  import Input from "../ui/input/input.svelte";
 
   interface Props {
     tags: Tag[];
@@ -22,20 +23,6 @@
     onValueChange(tagList);
   }
 
-  function onfocus(e: FocusEvent) {
-    e.preventDefault();
-    if (plus_button) return;
-    open = true;
-  }
-
-  function oninput(e: InputEvent) {
-    e.preventDefault();
-    if (!open) return;
-    filteredTags = initialList.filter((t) => {
-      if (t.label.match(searchText)) return t;
-    });
-  }
-
   let { tags, disabled = false, plus_button = true, max = 10, onValueChange = noop, wrap = true }: Props = $props();
 
   let open = $state(false);
@@ -45,7 +32,6 @@
   let filteredTags = $state(tags.slice(0, max));
   let searchText = $state("");
 
-  let inputWidth = $derived(plus_button ? "w-full" : "w-max");
   let wrapClass = $derived(wrap ? "flex-wrap" : "overflow-x-auto");
 
   $effect(() => {
@@ -53,52 +39,48 @@
   });
 </script>
 
-{#snippet searchInput()}
-  <Input placeholder="Search tags..." {onfocus} type="text" class={inputWidth} {oninput} bind:value={searchText} />
-{/snippet}
-
 <Card.Root>
   <div class="flex items-center gap-2 px-4 py-3 {wrapClass}">
     {#each tagList as tag}
       <BadgeTag {tag} handleClick={() => handleClick(tag)} />
     {/each}
     {#if !disabled}
-      <DropdownMenu.Root bind:open>
-        {#if plus_button}
-          <DropdownMenu.Trigger class={buttonVariants({ variant: "ghost", size: "icon" })}><span>&plus;</span></DropdownMenu.Trigger>
-        {:else}
-          {@render searchInput()}
-        {/if}
-
-        <DropdownMenu.Content class="w-64">
-          <DropdownMenu.Group>
+      <Popover.Root bind:open>
+        <Popover.Trigger>
+          {#if plus_button}
+            <Button variant="ghost">&plus;</Button>
+          {:else}
+            <Input placeholder="Search tags..." {onfocus} type="text" {oninput} bind:value={searchText} />
+          {/if}
+        </Popover.Trigger>
+        <Popover.Content>
+          <Command.Root class="w-64">
             {#if plus_button}
-              <DropdownMenu.Group>
-                {@render searchInput()}
-              </DropdownMenu.Group>
-              <DropdownMenu.Separator />
+              <Command.Input placeholder="Search tags..." />
             {/if}
-            <DropdownMenu.Group>
-              {#each filteredTags as suggestion}
-                <DropdownMenu.Item>
-                  <button
-                    class="flex h-8 cursor-pointer items-center gap-2 transition-colors hover:bg-muted"
-                    onclick={() => {
-                      tagList.push({
-                        label: suggestion.label,
-                        color: suggestion.color,
-                        disabled: false
-                      });
-                    }}>
-                    <div class="h-4 w-4 rounded-full border border-muted-foreground {suggestion.color}"></div>
-                    <span class="select-none">{suggestion.label}</span>
-                  </button>
-                </DropdownMenu.Item>
-              {/each}
-            </DropdownMenu.Group>
-          </DropdownMenu.Group>
-        </DropdownMenu.Content>
-      </DropdownMenu.Root>
+            <Command.List>
+              <Command.Group>
+                {#each filteredTags as suggestion}
+                  <Command.Item>
+                    <button
+                      class="flex h-8 cursor-pointer items-center gap-2 transition-colors hover:bg-muted"
+                      onclick={() => {
+                        tagList.push({
+                          label: suggestion.label,
+                          color: suggestion.color,
+                          disabled: false
+                        });
+                      }}>
+                      <div class="h-4 w-4 rounded-full border border-muted-foreground {suggestion.color}"></div>
+                      <span class="select-none">{suggestion.label}</span>
+                    </button>
+                  </Command.Item>
+                {/each}
+              </Command.Group>
+            </Command.List>
+          </Command.Root>
+        </Popover.Content>
+      </Popover.Root>
     {/if}
   </div>
 </Card.Root>
