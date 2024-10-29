708c0251da157d8861802fe7ed20f72687be9186
Matthew Davis <matthew@matthewdavis.io>
2024-10-29 14:03:00 -0500

    bump

--- cypress.config.ts
+++ cypress.config.ts
@@ -2,9 +2,17 @@ import { defineConfig } from "cypress";
 
 export default defineConfig({
   projectId: "n9kc57",
+
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
+    },
+  },
+
+  component: {
+    devServer: {
+      framework: "svelte",
+      bundler: "vite",
     },
   },
 });
